8d024fb5ac67c2c362b09476c82d04d2f4c45a94
min <minpaper@hanwha.com>
2025-10-10 18:01:03 +0900

    스크립트 수정

--- save_images4.sh
+++ save_images4.sh
@@ -6,8 +6,7 @@ mkdir -p $SAVE_DIR
 
 # 이미지 목록
 IMAGES=(
-    "harbor.hwgeneralins.com/plane/web:3.0.3"
-    "harbor.hwgeneralins.com/plane/space:3.0.3"
+    "harbor.hwgeneralins.com/plane/api:3.0.3"
 )
 
 echo "이미지 저장을 시작합니다..."
